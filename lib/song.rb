26afed8a1e90e2cabd85b6fb415262a863b0c004
Victoria Meng <nights1ststar@protonmail.com>
2018-03-23 00:05:03 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,7 +44,7 @@ class Song
 
   def self.new_from_filename(file)
     song = self.create
-    data = file.split
+    data = file.split(" - ")
     data[0].slice!(".mp3")
     song.name = data[1]
     song.artist_name = data[0]
